4b6a0cc5bf771b45c4f8635cf66e69ebe1d93dcc
CaptainVietnam6 <kiet.riley2005@gmail.com>
2022-02-22 14:41:46 -1000

    added matplotlib

--- main_calc.py
+++ main_calc.py
@@ -1,4 +1,5 @@
 from termcolor import colored
+from matplotlib import pyplot as plt
 
 print(colored("Calculator made by Kiet P.", "red"))
 print("The output of the calculator will indicate how many cycles\nit takes for the input value to reach back to 1")
@@ -7,16 +8,21 @@ print("If the value is even, it will be divided in half, if odd then 3n+1")
 def calc():
     input_num = int(input("Enter a number to run: "))
     value = input_num
-    count = 0
+    cycle_count = 0
+    cycle_list = []
     value_list = []
     while value != 1:
+        cycle_list.append(cycle_count)
+        value_list.append(value)
         if (value % 2) == 0: #if num is even
             value = round(value / 2)
         else: #if value is odd
             value = round((value * 3) + 1)
-        count += 1
-        value_list.append(value)
-    print(colored(f"The value {input_num} takes {count} cycles to come back to 1", "green"))
-    print(value_list)
+        cycle_count += 1
+    print(colored(f"The value {input_num} takes {cycle_count} cycles to come back to 1", "green"))
+    plt.plot(cycle_list, value_list)
+    plt.xlabel("Cycle")
+    plt.ylabel("Value")
+    plt.show()
 
 calc()
